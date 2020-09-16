e735d69f9a55d3de76ca232164f00ee20318c39c
FredHorizon <fredoliveira.sys@gmail.com>
2020-09-16 14:48:44 -0400

    Exemplifica o uso de asserção com Jest

--- sum.test.js
+++ sum.test.js
@@ -1,15 +1,6 @@
 'use strict'
 
-describe('# ESCOPO 1 de testes', () => {
-  it('Teste 1 com it', () => {})
-
-  test('Teste 2 com test', () => {})
-
-  it('Teste 3 com it pq it é usado por convenção', () => {})
-})
-
-describe('# ESCOPO 2 de testes', () => {
-  it('Teste 1', () => {})
-  it('Teste 2', () => {})
-  it('Teste 3', () => {})
+it('UM é igual a 1', () => {
+  // console.assert(1 === 2, '1 é diferente de 2')
+  expect(1).toBe(2)
 })
