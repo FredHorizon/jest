a805cc2a56af423db3cf1712cfbb0273d86352d2
FredHorizon <fredoliveira.sys@gmail.com>
2020-09-16 14:21:10 -0400

    Exemplifica o uso das funções no Jest

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,15 @@
 'use strict'
 
-it('Teste de jest', () => {
-  
+describe('# ESCOPO 1 de testes', () => {
+  it('Teste 1 com it', () => {})
+
+  test('Teste 2 com test', () => {})
+
+  it('Teste 3 com it pq it é usado por convenção', () => {})
+})
+
+describe('# ESCOPO 2 de testes', () => {
+  it('Teste 1', () => {})
+  it('Teste 2', () => {})
+  it('Teste 3', () => {})
 })
